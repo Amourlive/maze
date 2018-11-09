33e14090e3dc4318ee13c3635a5aa9add1200958
Aleksei Matskevich <aleksei_mackevich@outlook.com>
2018-11-10 02:59:41 +0300

    Simple Matrix version 2

--- matrix.rb
+++ matrix.rb
@@ -1,19 +1,39 @@
-# middleware consist array of array for ease of use
+# Implementation simple Matrix
+# represents middleware consists of an array of array for ease of use
 class Matrix
-  def initialize
-    @store = [[]]
+  def initialize(array_of_arrays = [[]])
+    raise ArgumentError, 'Argument is not array' unless
+        array_of_arrays.is_a? Array
+
+    array_of_arrays.each do |array|
+      raise ArgumentError, 'Argument array elements is not array' unless
+          array.is_a? Array
+    end
+    @store = array_of_arrays
   end
 
   def [](row, column)
-    return nil if @store[row].nil? || @store[column].nil?
+    return nil if @store[row].nil?
 
     @store[row][column]
   end
 
   def []=(row, column, obj)
-    @store[row] = [[]] if @store[row].nil?
-    @store[row][column] = [] if @store[row][column].nil?
+    @store[row] = [] if @store[row].nil?
     @store[row][column] = obj
+  end
+
+  def each(&block)
+    @store.each do |row|
+      row.each(&block)
+    end
+    self
+  end
+
+  def map(&block)
+    Matrix.new(@store.map do |row|
+      row.map(&block)
+    end)
   end
 
   def to_a
